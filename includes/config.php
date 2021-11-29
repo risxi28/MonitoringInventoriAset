8656d12626247c86edb59964a56ab387d7b8e782
risxi28 <71324565+risxi28@users.noreply.github.com>
2021-11-30 06:13:46 +0700

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,18 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| OWSA-INV V2
+|--------------------------------------------------------------------------
+| Author: Risxi Bagus Eriyana
+| Project Name: MIA MIC(monitoring inventory aset)
+
+|
+|
+|
+*/
+  define( 'DB_HOST', 'localhost' );          // Set database host
+  define( 'DB_USER', 'root' );             // Set database user
+  define( 'DB_PASS', '' );             // Set database password
+  define( 'DB_NAME', 'mia_db' );        // Set database name
+
+?>
